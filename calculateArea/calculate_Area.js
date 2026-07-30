731ff311261ce00ff4032d8cee142cc6bb8211c6
Avi <aviram.vijh@gmail.com>
2026-07-29 23:58:38 -0400

    grocery calculator

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,23 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 
 }
+
+function calculateGroceryTotal(amount1, amount2, amount3) {
+    const totalAmount = amount1 + amount2 + amount3;
+    return totalAmount;
+}
+
+function showGroceryTotal() {
+    const grocery1 = Number(document.getElementById("grocery1").value);
+    const grocery2 = Number(document.getElementById("grocery2").value);
+    const grocery3 = Number(document.getElementById("grocery3").value);
+
+    const total = calculateGroceryTotal(
+        grocery1,
+        grocery2,
+        grocery3
+    );
+
+    document.getElementById("total").textContent =
+        `Total grocery expenditure: £${total.toFixed(2)}`;
+}
